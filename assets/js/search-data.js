26e2506ec463ad2663ea2a2153f091d3fe753a6f
hannawang09 <hannawang09@gmail.com>
2025-06-04 03:21:54 +0000

    Deploying to gh-pages from @ hannawang09/hannawang09.github.io@542adbd439eacbca600fc7ddd8bea538db64cb34 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "You can download my resume by clicking the button on the right.",
